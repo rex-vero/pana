41b5c817f2e08ac3831084da8672e29d5bcbc0c8
rex-vero <142157808+rex-vero@users.noreply.github.com>
2024-05-05 22:04:33 +0330

    bootstarp, scss for css, fonts, images & js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,14 @@
 const support = document.getElementById('support');
 const pay = document.getElementById('pay');
+const smPay = document.getElementById('pay-sm');
 const month = document.getElementById('month');
-window.addEventListener('scroll', () => {
-    const nav = document.getElementById('nav');
-    scrollY >= 30 ? nav.classList.add('nav-active') : nav.classList.remove('nav-active');
-});
-support.addEventListener('click', () => {
-    const icons = document.querySelectorAll('.links');
-    for (const icon of icons) icon.classList.toggle('scale');
-});
+const smMonth = document.getElementById('month-sm');
 let sum = 100000;
 let num = 3;
-const calc = (pay, month, sum, num) => {
-    const prices = document.querySelectorAll('.price');
-    const payToman = document.getElementById('pay-toman');
-    const months = document.getElementById('months');
+const calc = (textPay, textMon, textPrice, pay, month, sum, num) => {
+    const prices = document.querySelectorAll(`.${textPrice}`);
+    const payToman = document.getElementById(textPay);
+    const months = document.getElementById(textMon);
     const dash = input => input.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     months.textContent = month;
     payToman.textContent = dash(pay);
@@ -24,9 +18,23 @@ const calc = (pay, month, sum, num) => {
         price.textContent = sum;
     }
 }
+window.addEventListener('scroll', () => {
+    const nav = document.getElementById('nav');
+    scrollY >= 30 ? nav.classList.add('nav-active') : nav.classList.remove('nav-active');
+});
+support.addEventListener('click', () => {
+    const icons = document.querySelectorAll('.links');
+    for (const icon of icons) icon.classList.toggle('scale');
+});
 pay.addEventListener('input', () => {
-    calc(pay.value, month.value, sum, num);
+    calc('pay-toman', 'months', 'price', pay.value, month.value, sum, num);
 });
 month.addEventListener('input', () => {
-    calc(pay.value, month.value, sum, num);
+    calc('pay-toman', 'months', 'price', pay.value, month.value, sum, num);
+});
+smPay.addEventListener('input', () => {
+    calc('pay-toman-sm', 'months-sm', 'price-sm', smPay.value, smMonth.value, sum, num);
+});
+smMonth.addEventListener('input', () => {
+    calc('pay-toman-sm', 'months-sm', 'price-sm', smPay.value, smMonth.value, sum, num);
 });
